2f8c25f7db892f0a07737915d6892e954e5eaa71
svale <isvalina@veleri.hr>
2024-12-05 11:06:18 +0100

    sve valjda

---
+++ frontend/quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Moj Naslov</h1>
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
